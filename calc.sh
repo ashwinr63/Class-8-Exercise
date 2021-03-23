4dae05dc962eedb86a439afe1d1d66af57b5912a
Ashwin Ramkumar <8831182+ashwinr63@users.noreply.github.com>
2021-03-22 21:39:40 -0400

    Adding the Menu and operations

--- calc.sh
+++ calc.sh
@@ -17,3 +17,33 @@ echo "Welcome to my Calculation"
 res=0
 process="y"
 
+# Getting the Menu option for operation.
+while [$process = "y"]
+do
+echo "1.Add"
+echo "2.Subtract"
+echo "3.Multiply"
+echo "4.Divide"
+read choice
+case $choice in
+1)res=$(echo "$a + $b" | bc - 1)
+echo "After Addition =" $res
+;;
+2)res=$(echo "$a - $b" | bc - 1)
+echo "By Subtracting =" $res
+;;
+3)res=$(echo "$a * $b" | bc - 1)
+echo "Product is =" $res
+;;
+4)res=$(echo "$a / $b" | bc - 1)
+echo "Division value is =" $res
+;;
+*) echo "Your Choice is Invalid"
+esac
+echo "Please press y if you want to Continue"
+read process
+if [$process !="y"]
+then
+exit
+fi
+done
